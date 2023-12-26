57301fcd6eee8cdb87abdef87e1b7ba21fe76f07
naipaka <naipakapaka@gmail.com>
2023-12-26 16:21:18 +0900

    fix: Renamed 'configurator' to match package name 'altfire_configurator

--- packages/altfire_configurator/example/lib/main.dart
+++ packages/altfire_configurator/example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:altfire_configurator/configurator.dart';
+import 'package:altfire_configurator/altfire_configurator.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
---
+++ packages/altfire_configurator/lib/altfire_configurator.dart
@@ -0,0 +1,5 @@
+/// altfire_configurator with FlutterFire Remote Config.
+library altfire_configurator;
+
+export 'src/config.dart';
+export 'src/configurator.dart';
