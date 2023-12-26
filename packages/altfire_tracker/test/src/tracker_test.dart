1aaf6a9f25f41caddc2284a554890f8a0bdce0dc
naipaka <naipakapaka@gmail.com>
2023-12-26 19:14:03 +0900

    fix: Add altfire_tracker library and update import in tracker_test

--- packages/altfire_tracker/test/src/tracker_test.dart
+++ packages/altfire_tracker/test/src/tracker_test.dart
@@ -1,6 +1,6 @@
 import 'dart:isolate';
 
-import 'package:altfire_tracker/tracker.dart';
+import 'package:altfire_tracker/altfire_tracker.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
