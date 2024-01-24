9f3c6d434cdcfba98f041fa1d326ba9e8a07fcd7
Ryunosuke Muramatsu <muramatsu@altive.co.jp>
2024-01-24 11:54:03 +0900

    chore(altfire_authenticator): change file name from authenticator to altfire_authenticator

--- packages/altfire_authenticator/lib/src/apple_authenticator.dart
+++ packages/altfire_authenticator/lib/src/apple_authenticator.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 
-import '../authenticator.dart';
+import '../altfire_authenticator.dart';
 import 'authenticatable.dart';
 
 class AppleAuthenticator implements Authenticatable {
--- packages/altfire_authenticator/test/authenticator_test.dart
+++ packages/altfire_authenticator/test/authenticator_test.dart
@@ -1,4 +1,4 @@
-import 'package:altfire_authenticator/authenticator.dart';
+import 'package:altfire_authenticator/altfire_authenticator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 
