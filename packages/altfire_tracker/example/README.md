7547028c79ebdee197deb3abe5a05ca5f18c2372
nkmr <75433035+k-nkmr@users.noreply.github.com>
2024-01-24 11:28:09 +0900

    feat : Create an example package for altfire_tracker (#51)
    * Create an example package for altfire_tracker
    
    * fix: add MaterialApp
    
    * add GoogleService-Info.plist to pbxproj section
    
    * build: add GoogleService-Info.plist for macOS
    
    * docs: Update README.md
    
    * chore: Add launch configuration for altfire_tracker/example
    
    * build: flutter create diff
    
    * add new usage
    
    * modified buttons
    
    * modified AndroidManifest.xml
    
    * Modified gitignore and readme.
    
    * Modified pubspec.yaml
    
    ---------
    
    Co-authored-by: Ryunosuke Muramatsu <muramatsu@altive.co.jp>

---
+++ packages/altfire_tracker/example/README.md
@@ -0,0 +1,3 @@
+# altfire_tracker_example
+
+Demonstrates how to use the altfire_tracker plugin.
---
+++ packages/altfire_tracker/example/lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:altfire_tracker/altfire_tracker.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final tracker = Tracker();
+  FlutterError.onError = tracker.onFlutterError;
+  PlatformDispatcher.instance.onError = tracker.onPlatformError;
+  Isolate.current.addErrorListener(tracker.isolateErrorListener());
+  await tracker.setUserId('example_user_id');
+
+  runApp(
+    MaterialApp(
+      home: HomePage(tracker: tracker),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    super.key,
+    required this.tracker,
+  });
+
+  final Tracker tracker;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('altfire_tracker example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              child: const Text('Log event'),
+              onPressed: () {
+                unawaited(
+                  tracker.logEvent(
+                    'example_event',
+                    parameters: {
+                      'example_key': 'example_value',
+                    },
+                  ),
+                );
+              },
+            ),
+            FilledButton(
+              onPressed: () {
+                try {
+                  throw Exception('example exception');
+                } on Exception catch (e, stackTrace) {
+                  unawaited(tracker.recordError(e, stackTrace));
+                }
+              },
+              child: const Text('Record Error'),
+            ),
+            FilledButton(
+              onPressed: () {
+                throw FlutterError('example unhandled exception');
+              },
+              child: const Text('Record unhandled Exception'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
