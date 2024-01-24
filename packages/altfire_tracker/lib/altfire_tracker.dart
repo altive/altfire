ac7f53649aa9f59eb9cc4660eb23612c5d910c31
Naoya Miyagawa <73296055+makio0325@users.noreply.github.com>
2024-01-24 19:21:54 +0900

    feat: Export MethodChannelFirebaseCrashlytics (#65)

--- packages/altfire_tracker/lib/altfire_tracker.dart
+++ packages/altfire_tracker/lib/altfire_tracker.dart
@@ -3,3 +3,5 @@ library altfire_tracker;
 
 export 'src/trackable.dart';
 export 'src/tracker.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart'
+    show MethodChannelFirebaseCrashlytics;
