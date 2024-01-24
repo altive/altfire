514896a85ddc0efe3e65eb346ee7995053093054
makio0325 <n.miyagawa@spella.tech>
2024-01-24 19:33:17 +0900

    fix: Change export file

--- packages/altfire_tracker/lib/altfire_tracker.dart
+++ packages/altfire_tracker/lib/altfire_tracker.dart
@@ -3,5 +3,5 @@ library altfire_tracker;
 
 export 'src/trackable.dart';
 export 'src/tracker.dart';
-export 'package:firebase_crashlytics/firebase_crashlytics.dart'
+export 'package:firebase_crashlytics_platform_interface/firebase_crashlytics_platform_interface.dart'
     show MethodChannelFirebaseCrashlytics;
