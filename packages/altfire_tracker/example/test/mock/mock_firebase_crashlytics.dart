1471b8244af2cbe924414627a4eb878c552efcda
makio0325 <n.miyagawa@spella.tech>
2024-01-25 14:48:03 +0900

    fix info

--- packages/altfire_tracker/example/test/mock/mock_firebase_crashlytics.dart
+++ packages/altfire_tracker/example/test/mock/mock_firebase_crashlytics.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:altfire_tracker/altfire_tracker.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter/services.dart';
--- packages/altfire_tracker/lib/altfire_tracker.dart
+++ packages/altfire_tracker/lib/altfire_tracker.dart
@@ -1,7 +1,8 @@
 /// altfire_tracker with FlutterFire Analytics and Crashlytics.
 library altfire_tracker;
 
-export 'src/trackable.dart';
-export 'src/tracker.dart';
 export 'package:firebase_crashlytics_platform_interface/firebase_crashlytics_platform_interface.dart'
     show MethodChannelFirebaseCrashlytics;
+
+export 'src/trackable.dart';
+export 'src/tracker.dart';
