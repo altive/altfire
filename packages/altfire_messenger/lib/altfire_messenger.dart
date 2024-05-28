ac72d6ec97b253e1615e43e4627ca04f52a51fe1
Ryota Kobayashi <45661924+naipaka@users.noreply.github.com>
2024-05-29 08:51:50 +0900

    build: Delete unnecessary library names (#89)

--- packages/altfire_messenger/lib/altfire_messenger.dart
+++ packages/altfire_messenger/lib/altfire_messenger.dart
@@ -1,6 +1,6 @@
 /// altfire_messenger with FlutterFire Messaging, Local Notifications,
 /// In-App Messaging, and Firebase Installation ID.
-library altfire_messenger;
+library;
 
 export 'package:firebase_messaging/firebase_messaging.dart'
     show AuthorizationStatus;
